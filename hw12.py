4c48c962f85f2c577fb57fc8f4411e8b0ba9636b
Daniel Chapin <dchapes123@gmail.com>
2024-12-10 20:59:21 -0500

    Problem 2 Dataset loading

--- hw12.py
+++ hw12.py
@@ -1,4 +1,28 @@
-import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+from pathlib import Path
+from torch import tensor
 
-print("Hello, world!")
-print(torch)
+#############
+# Problem 2 #
+#############
+
+
+class MNIST_dataset(Dataset):
+    datapath: Path
+    paths: list[Path]
+
+    def __init__(self, datapath):
+        super().__init__()
+        self.datapath = Path(datapath)
+        self.paths = list(self.datapath.rglob("*.png"))
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index].as_posix()
+        image = read_image(path).squeeze(dim=0) / 255
+        label = int(path.split('/')[-2])
+
+        return image, tensor(label)
